43f914b741ab570f58b6b4a9227b01586d7ce5f2
Eliseev Vadim <55801514+EliseevVadim@users.noreply.github.com>
2021-03-19 23:04:35 +0300

    Lab13

---
+++ Lab13_Course2/Lab13_Course2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Lab13_Course2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(708, 532);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 550);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(740, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(263, 603);
+            this.panel1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(137, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(99, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите вершины полигона";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите число вершин";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(464, 550);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 41);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Автоматическая генерация";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1003, 603);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(1019, 642);
+            this.MinimumSize = new System.Drawing.Size(1019, 642);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №13";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab13_Course2/Lab13_Course2/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab13_Course2
+{
+    public partial class Form1 : Form
+    {
+        const int NMAX = 500;
+        const double BIG = 1.0e30;
+        Graphics graphics; 
+        Pen pen;
+        int n; 
+        int[] v; 
+        double[] x; 
+        double[] y;
+        ComboBox tbComboBox1;
+        public const int bh = 40; 
+        public const int bw = 40;
+        public static int iRows = 20, iColumns = 2;
+        public TextBox[,] tbArray;
+        public Form1()
+        {
+            InitializeComponent();
+            v = new int[NMAX];
+            x = new double[NMAX];
+            y = new double[NMAX];
+            graphics = pictureBox1.CreateGraphics();
+            pen = new Pen(Brushes.Black, 2);
+            tbComboBox1 = new ComboBox()
+            {
+                Location = new Point(61, 31),
+                Width = 121,
+                Height = 21
+            };
+            panel1.Controls.Add(tbComboBox1);
+            for (int i = 0; i < NMAX; i++) 
+            { 
+                tbComboBox1.Items.Add(20+i); 
+            }
+            tbComboBox1.SelectedItem = 20;
+            tbComboBox1.SelectedIndexChanged += new EventHandler(UpdateBoxes);
+            CreateBoxes(iRows, iColumns);
+        }
+        private void UpdateBoxes(object sender, EventArgs e)
+        {
+            CreateBoxes(Convert.ToInt32(tbComboBox1.SelectedItem), 2);
+            panel1.Update();
+        }
+        public void CreateBoxes(int rows, int columns)
+        {
+            tbArray = new TextBox[rows, columns]; 
+            int y1 = 80;
+            for (int i = 0; i < rows; i++)
+            {
+                int x1 = 90; y1 += bh - 10;
+                for (int j = 0; j < columns; j++)
+                {
+                    tbArray[i, j] = new TextBox(); 
+                    tbArray[i, j].Name = "TextBox" + i + j;
+                    panel1.Controls.Add(tbArray[i, j]);
+                    tbArray[i, j].SetBounds(x1, y1, bw, bh);
+                    x1 += bw;
+                }
+                tbArray[i, 0].Text = x[i].ToString("R");
+                tbArray[i, 1].Text = y[i].ToString("R");
+            }
+        }
+        private int IX(double x)
+        { 
+            double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5; 
+            return (int)xx; 
+        }
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5;
+            return (int)yy;
+        }
+        private void Draw(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1), IY(y1)); Point point2 = new Point(IX(x2), IY(y2));
+            graphics.DrawLine(pen, point1, point2);
+        }
+        private unsafe bool CounterClock(int h, int i, int j, double* pdist)
+        {
+            double xh = x[v[h]], xi = x[v[i]], xj = x[v[j]],
+            yh = y[v[h]], yi = y[v[i]], yj = y[v[j]],
+            x_hi, y_hi, x_hj, y_hj, Determ;
+            x_hi = xi - xh; y_hi = yi - yh; x_hj = xj - xh; y_hj = yj - yh;
+            *pdist = x_hj * x_hj + y_hj * y_hj;
+            Determ = x_hi * y_hj - x_hj * y_hi;
+            return (Determ > 1e-6);
+        }
+        private void DrawPolygon()
+        {
+            int i; double xold, yold;
+            xold = x[n - 1]; yold = y[n - 1];
+            for (i = 0; i < n; i++)
+            {
+                Draw(xold, yold, x[i], y[i]); xold = x[i]; yold = y[i];
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Triangulate(false);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool finish = true;
+            while (finish)
+            {
+                try
+                {
+                    Triangulate(true);
+                    finish = false;
+                }
+                catch
+                {
+                    pen.DashStyle = DashStyle.Solid;
+                }
+            }
+            MessageBox.Show("c'est magnifique!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private unsafe void Triangulate(bool randomly)
+        {
+            graphics.Clear(Color.White);
+            int i, h, j, m, k, imin = 0;
+            double diag, min_diag;
+            n = Convert.ToInt16(tbComboBox1.SelectedItem.ToString());
+            if (n >= NMAX)
+            {
+                MessageBox.Show("Количество вершин слишком велико!", "Ошибка!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            Random random = new Random();
+            for (i = 0; i < n; i++)
+            {
+                if (randomly)
+                {
+                    tbArray[i, 0].Text = (random.Next(0, 9) + random.NextDouble()).ToString();
+                    tbArray[i, 1].Text = (random.Next(0, 6) + random.NextDouble()).ToString();
+                }
+                x[i] = Convert.ToDouble(tbArray[i, 0].Text);
+                y[i] = Convert.ToDouble(tbArray[i, 1].Text);
+                v[i] = i;
+            }
+            m = n;
+            DrawPolygon();
+            float[] dashValues = { 5, 5, 5, 5 };
+            pen.DashPattern = dashValues;
+            while (m > 3)
+            {
+                min_diag = BIG;
+                for (i = 0; i < m; i++)
+                {
+                    h = (i == 0 ? m - 1 : i - 1); j = (i == m - 1 ? 0 : i + 1);
+                    if (CounterClock(h, i, j, &diag) && (diag < min_diag))
+                    {
+                        min_diag = diag; imin = i;
+                    }
+                }
+                i = imin; h = (i == 0 ? m - 1 : i - 1);
+                j = (i == m - 1 ? 0 : i + 1);
+                if (min_diag == BIG)
+                {
+                    if (!randomly)
+                    {
+                        MessageBox.Show("Неправильное направление обхода!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);                        
+                    }
+                    throw new Exception();
+                }
+                Draw(x[v[h]], y[v[h]], x[v[j]], y[v[j]]);
+                m--;
+                for (k = i; k < m; k++)
+                {
+                    v[k] = v[k + 1];
+                }
+            }
+            pen.DashStyle = DashStyle.Solid;
+        }
+    }
+}
