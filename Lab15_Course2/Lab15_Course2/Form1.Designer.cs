ccc616a842350a6fd39b6cc1ab6ec632a35cbfbe
Eliseev Vadim <eliseevv02@mail.ru>
2021-04-10 23:01:15 +0300

    Lab15

---
+++ Lab15_Course2/Lab15_Course2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Lab15_Course2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 393);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(221, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(567, 393);
+            this.trackBar2.Maximum = 360;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(221, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Угол θ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(745, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Угол φ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 346);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №15";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab15_Course2/Lab15_Course2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab15_Course2
+{
+    public struct Point3D
+    {
+        public int X;
+        public int Y;
+        public int Z;
+    }
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        int height = 50;
+        Point3D[] vertices;
+        double v11, v12, v13, v21, v22, v23, v32, v33, v43;
+        double rho = 250.0, theta = 320.0, phi = 45.0;
+        double c1 = 5.0, c2 = 3.5;
+        double screenDist = 5;
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            phi = trackBar2.Value;
+            CalculateCoefficients(rho, theta, phi);
+            DrawCube(height);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CalculateCoefficients(rho, theta, phi);
+            DrawCube(height);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            theta = trackBar1.Value;
+            CalculateCoefficients(rho, theta, phi);
+            DrawCube(height);
+        }
+        private int IX(double x)
+        {
+            double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5;
+            return (int)xx;
+        }
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5;
+            return (int)yy;
+        }
+        private void DrawCube(double h)
+        {
+            DrawPerspectiveLine(h, -h, -h, h, h, -h);
+            DrawPerspectiveLine(h, h, -h, -h, h, -h);
+            DrawPerspectiveLine(-h, h, -h, -h, h, h);
+            DrawPerspectiveLine(-h, h, h, -h, -h, h);
+            DrawPerspectiveLine(-h, -h, h, h, -h, h);
+            DrawPerspectiveLine(h, -h, h, h, -h, -h);
+            DrawPerspectiveLine(h, h, -h, h, h, h);
+            DrawPerspectiveLine(h, h, h, -h, h, h);
+            DrawPerspectiveLine(h, h, h, h, -h, h);
+            DrawPerspectiveLine(h, -h, -h, -h, -h, -h);
+            DrawPerspectiveLine(-h, -h, -h, -h, h, -h);
+            DrawPerspectiveLine(-h, -h, -h, -h, -h, h); 
+        }
+        private void DrawPerspectiveLine(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double X1 = 0, Y1 = 0, X2 = 0, Y2 = 0;
+            Perspective(x1, y1, z1, ref X1, ref Y1);
+            Perspective(x2, y2, z2, ref X2, ref Y2);
+            Point point1 = new Point(IX(X1), IY(Y1));
+            Point point2 = new Point(IX(X2), IY(Y2));
+            graphics.DrawLine(Pens.Black, point1, point2);
+        }
+        private void CalculateCoefficients(double rho, double theta, double phi)
+        {
+            double th, ph, coeff, costh, sinth, cosph, sinph;
+            coeff = Math.PI / 180;
+            th = theta * coeff;
+            ph = phi * coeff;
+            costh = Math.Cos(th);
+            sinth = Math.Sin(th);
+            cosph = Math.Cos(ph);
+            sinph = Math.Sin(ph);
+            v11 = -sinth;
+            v12 = -cosph * costh;
+            v13 = -sinph * costh;
+            v21 = costh;
+            v22 = -cosph * sinth;
+            v23 = -sinph * sinth;
+            v32 = sinph;
+            v33 = -cosph;
+            v43 = rho;
+        }
+        private void Perspective(double x, double y, double z, ref double pX, ref double pY)
+        {
+            double xe, ye, ze;
+            xe = v11 * x + v21 * y;
+            ye = v12 * x + v22 * y + v32 * z;
+            ze = v13 * x + v23 * y + v33 * z + v43;
+            pX = screenDist * xe / ze + c1;
+            pY = screenDist * ye / ze + c2;
+        }
+    }
+}
